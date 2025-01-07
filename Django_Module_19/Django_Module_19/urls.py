0b55f0d36de496688ad78cbe9490cb902bc7678e
Jasper.IPG <jasper.ipg@gmail.com>
2025-01-07 19:16:37 +0600

    'Время объединять'

--- Django_Module_19/Django_Module_19/urls.py
+++ Django_Module_19/Django_Module_19/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Django_Module_19 project.
+URL configuration for UrbanDjango project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -14,9 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from task1.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', main_page),
+    path('NS/', switch_games),
+    path('cart/', cart),
+    path('registration/', registration),
 ]
--- Django_Module_19/task1/views.py
+++ Django_Module_19/task1/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
+from .models import Buyer, Game
+from .forms import UserRegister
+
 
 # Create your views here.
+def main_page(request):
+    return render(request, 'main_page.html')
+
+
+def switch_games(request):
+    title = 'Игры Nintendo'
+    games = [game for game in Game.objects.all()]
+    context = {
+        'title': title,
+        'games': games
+    }
+    return render(request, 'games.html', context)
+
+
+def cart(request):
+    title = 'Корзина'
+    text = 'Извините, Ваша корзина пуста'
+    context = {
+        'title': title,
+        'text': text
+    }
+    return render(request, 'shopping_cart.html', context)
+
+
+def registration(request):
+    usernames = [buyer.name for buyer in Buyer.objects.all()]
+    info = {}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get("password")
+        repeat_password = request.POST.get("repeat_password")
+        age = int(request.POST.get('age'))
+
+        if username in usernames:
+            info['error'] = "Пользователь уже существует"
+        elif password != repeat_password:
+            info['error'] = 'Пароли не совпадают'
+        elif age < 18:
+            info['error'] = 'Вы должны быть старше 18'
+        elif password == repeat_password and username not in usernames:
+            Buyer.objects.create(name=username, balance=5000, age=age)
+            info['message'] = f'Приветствуем, {username}!'
+    return render(request, 'registration_page.html', context=info)
